ef3b9dba8ffc4eb3ae9d866b4756f53ef80d2474
Benjamin <muhlestein1@gmail.com>
2022-06-13 20:48:15 -0700

    Added Code

--- Fly-dog.js
+++ Fly-dog.js
@@ -1,8 +1,8 @@
 // Size of Dog Character 
 
-document.getElementById("Dog").style.width= "100px";  
+document.getElementById("Dog").style.width= "200px";
 
-// Make image Fly 
+// Make Dog Character Move 
 const EL_img = document.querySelector("#Dog");
 const pos = {x:0, y:0};
 
@@ -14,20 +14,24 @@ document.addEventListener("keydown", (ev) => {
   ev.preventDefault(); // Prevent Browser scroll if overflow
 
   ({
-    Left:  () => pos.x -= 5,
-    Right: () => pos.x += 5,
-    Up:    () => pos.y -= 5,
-    Down:  () => pos.y += 5,
+    Left:  () => pos.x -= 70,
+    Right: () => pos.x += 70,
+    Up:    () => pos.y -= 70,
+    Down:  () => pos.y += 70,
   }[dir])(); 
   
   EL_img.style.transform = `translate(${pos.x}px, ${pos.y}px)`
 });
-
-
         
 // Background Size 
+document.getElementById("Background").style.width= "3000px";
 
-document.getElementById("Background").style.width= "2000px";
+//Audio file
+
+//Maze 
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### Dog Picture Attribution Link: 'https://www.freepik.com/vectors/shiba-inu'>Shiba inu vector created by catalyststuff - www.freepik.com
 ### Background Image Attribution Link:
  'https://www.freepik.com/vectors/wild-west'>Wild west vector created by upklyak - www.freepik.com
+ ### Game Over Screen Link:
+ https://www.freepik.com/vectors/game'>Game vector created by macrovector - www.freepik.com
